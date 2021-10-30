6c9b92e76232fec5bef1091b77921a1a1570d23a
djosedonado <djosedonado@unicesar.edu.co>
2021-10-30 11:40:38 -0500

    Confirmacion Capa Entidad

---
+++ Entidad/Contabilizacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Contabilizacion : Usuario
+    {
+        public Contabilizacion()
+        {
+            Debe = 0;
+            Haber = 0;
+            Saldo = 0;
+
+        }
+        public string Id { get; set; }
+        public string CodigoCuenta { get; set; }
+        public string Detalle { get; set; }
+        public double Debe { get; set; }
+        public double Haber{ get; set; }
+        public double Saldo { get; set; }
+        public double SumatoriaDebe { get; set; }
+        public double SumatoriaHaber { get; set; }
+        public DateTime fechaRegistroContable { get; set; }
+
+        private void CalcularSaldo()
+        {
+            Saldo = Debe - Haber;
+        }
+
+        private void CalcularSumatoriaDebe()
+        {
+            SumatoriaDebe = SumatoriaDebe + Debe;
+        }
+
+        private void CalcularSumatoriaHaber()
+        {
+            SumatoriaHaber = SumatoriaHaber + Haber;
+        }
+
+    }
+}
---
+++ Entidad/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Usuario
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+
+    }
+}
